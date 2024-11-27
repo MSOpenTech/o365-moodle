c3d4b5fca75a9acf073012b8c78e9aeaf2737108
Lai Wei <lai.wei@enovation.ie>
2024-11-27 12:21:23 +0000

    Log auth_oidc user out from all sessions when Front-channel logout URL is triggered

--- auth/oidc/classes/task/cleanup_oidc_sid.php
+++ auth/oidc/classes/task/cleanup_oidc_sid.php
@@ -15,27 +15,35 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Event observers.
+ * A scheduled task to clean up oidc sid records.
  *
  * @package auth_oidc
- * @author James McQuillan <james.mcquillan@remote-learner.net>
+ * @author Lai Wei <lai.wei@enovation.ie>
  * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- * @copyright (C) 2014 onwards Microsoft, Inc. (http://microsoft.com/)
+ * @copyright (C) 2021 onwards Microsoft, Inc. (http://microsoft.com/)
  */
 
-defined('MOODLE_INTERNAL') || die();
+namespace auth_oidc\task;
 
-$observers = [
-    [
-        'eventname' => '\core\event\user_deleted',
-        'callback' => '\auth_oidc\observers::handle_user_deleted',
-        'priority' => 200,
-        'internal' => false,
-    ],
-    [
-        'eventname' => '\core\event\user_loggedout',
-        'callback' => '\auth_oidc\observers::handle_user_loggedout',
-        'priority' => 200,
-        'internal' => false,
-    ],
-];
+use core\task\scheduled_task;
+
+/**
+ * A scheduled task that cleans up OIDC SID records.
+ */
+class cleanup_oidc_sid extends scheduled_task {
+    /**
+     * Get a descriptive name for the task.
+     */
+    public function get_name() {
+        return get_string('task_cleanup_oidc_sid', 'auth_oidc');
+    }
+
+    /**
+     * Clean up OIDC SID records.
+     */
+    public function execute() {
+        global $DB;
+
+        $DB->delete_records_select('auth_oidc_sid', 'timecreated < ?', [strtotime('-1 day')]);
+    }
+}
